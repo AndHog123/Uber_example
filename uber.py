8bdee94e387531978adbf8048121dcefeb462b15
AndHog123 <sba25161@student.cct.ie>
2026-05-12 19:28:28 +0100

    Update uber.py

--- uber.py
+++ uber.py
@@ -5,11 +5,11 @@ import numpy as np
 st.title("Uber Pickups in NY")
 
 DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
          
 def load_data(nrows):
-        data = pd.read_csv(DATA_URL, nrows = nrows)
+        data = pd.read_csv(uploaded_files)
         lowercase = lambda x:str(x).lower()
         data.rename(lowercase, axis = "columns", inplace = True)
         data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
